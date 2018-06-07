25c413bc8d0d799fb80113fce180beebe68bc8d4
walisonjose <walison.josededeus@gmail.com>
2018-06-07 10:05:23 -0300

    Create verifcaphp.sh

---
+++ tests/verifcaphp.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+var=$(php -v)
+
+versaophp=${var:4:1}
+
+if [ "$versaophp" = "7" ]
+ then
+   echo "Versão 7!\n Instalando a versão do PHPUnit compatível!"
+   sudo wget https://phar.phpunit.de/phpunit-6.5.7.phar -P /tmp
+   sudo chmod +x /tmp/phpunit-6.5.7.phar
+   sudo mv /tmp/phpunit-6.5.7.phar /usr/local/bin/phpunit
+ else
+   echo "Outra versão! Mantendo a instalação padrão!"
+fi

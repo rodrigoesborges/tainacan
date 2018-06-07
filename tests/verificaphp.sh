93424b8cee8661a3953908cba9d9c492cc04119b
walisonjose <walison.josededeus@gmail.com>
2018-06-07 14:59:07 -0300

    Update verificaphp.sh

--- tests/verificaphp.sh
+++ tests/verificaphp.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-var=`php --version`
+var="$(php -v)"
 versaophp=${var:4:1}
-if [ "$versaophp" = "7" ]
- then
+if [ "$versaophp" = "7" ]; then
    echo "Versão 7!\n Instalando a versão do PHPUnit compatível!"
    sudo wget https://phar.phpunit.de/phpunit-6.5.7.phar -P /tmp
    sudo chmod +x /tmp/phpunit-6.5.7.phar
    sudo mv /tmp/phpunit-6.5.7.phar /usr/local/bin/phpunit
- else 
+else 
    echo "Outra versão! Mantendo a instalação padrão!"  
 fi
